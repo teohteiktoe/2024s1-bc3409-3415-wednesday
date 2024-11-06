0eafd033483d374cc510c4b048aa9ac7b611d9a8
Teoh TeikToe <teohteiktoe@gmail.com>
2024-11-06 15:14:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask,render_template,request
 import google.generativeai as genai
 import os
 import textblob
+import sqlite3
+import datetime
 
-api = os.getenv("MAKERSUITE_API_KEY")
+
+#os.environ["MAKERSUITE_API_KEY] = ""
+api = "AIzaSyCFIL-2qRWHrUqzyf_TN3A5IKQsVgB2zHg"
 genai.configure(api_key=api)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
@@ -12,6 +16,18 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main", methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    currentDateTime = datetime.datetime.now()
+    conn = sqlite3.connect('userlog.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO user (name,timestamp) VALUES(?,?)',(q,currentDateTime))
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("main.html"))
 
 @app.route("/ai_agent", methods=["GET","POST"])
 def ai_agent():
@@ -41,5 +57,19 @@ def SA_reply():
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_reply.html",r=r))
 
+@app.route("/userlog", methods=["GET","POST"])
+def userlog():
+    conn = sqlite3.connect('userlog.db')
+    c = conn.cursor()
+    c.execute('select * From user')
+    r=""
+    for row in c:
+        print(row)
+        r = r + str(row)
+    c.close()
+    conn.close()
+    return(render_template("userlog.html",r=r))
+
 if __name__ == "__main__":
     app.run()
+
